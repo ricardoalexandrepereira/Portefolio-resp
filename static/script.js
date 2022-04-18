eb4e0691b82414218d378c84b6cbd552ad7fff55
ricardoalexandrepereira <ricardo_certe@hotmail.com>
2022-04-18 13:57:00 +0100

    sobre e habilidades

--- static/script.js
+++ static/script.js
@@ -10,3 +10,15 @@ menuMobile.addEventListener('click',()=>{
     body.classList.toggle('menu-nav-active')
 
 })
+/*fecha o menu quando clicar em algum item e muda o icon para list*/
+
+const navItem = document.querySelectorAll('.nav-item')
+
+navItem.forEach(item =>{
+    item.addEventListener('click',()=>{
+        if(body.classList.contains('menu-nav-active')){
+            body.classList.remove('menu-nav-active')
+            menuMobile.classList.replace('bi-x','bi-list')
+        }
+    })
+})
